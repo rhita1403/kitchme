bc9a61ba55459c03cb36363f5b6460c1c90b1755
william varsovie <william.varsovie@gmail.com>
2023-11-23 12:12:22 +0100

    terminé

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "dashboard", to: "pages#dashboard"
+  get "dashboard", to: "pages#dashboard", as: 'dashboard'
 
   resources :kitchens, only: [:index, :new, :create, :destroy, :show, :edit, :update] do
     resources :bookings, only: [:new, :create]
